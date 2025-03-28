feabeddb95592a937e68ac39d39a01d28ec0feae
Javier Gómez <franciscog@thisisyr.com>
2025-03-28 12:15:54 -0500

    Add RBN header

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RbnHeader from './components/rbn-header';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <RbnHeader />
     <BrowserRouter>
       <App />
     </BrowserRouter>
