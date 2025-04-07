44a3044b721578c8f43593817720e5f24680a601
Javier Gómez <franciscog@thisisyr.com>
2025-04-07 14:21:12 -0500

    Add menu for dektop

--- src/components/menu/components/attribute-header/index.tsx
+++ src/components/menu/components/attribute-header/index.tsx
@@ -3,71 +3,74 @@ import { useState, memo } from 'react';
 
 
 import './index.css';
+import { ICAMap, IAttributeValue } from '../../../../constants';
+import { useConfigure } from '../../../../providers/configure-core';
 
 //import AttributeSelector from '../attribute-selector';
 
 interface IAttributeHeaderPropTypes {
   onClick: Function;
-  //caInfo: ICAMap
+  caInfo: ICAMap
 };
 
 const AttributeHeader = memo(function (props: IAttributeHeaderPropTypes) {
   const [menuOpen, setMenuOpen] = useState(false);
-  /*const { configureService } = useConfigure();
+  const { configureCoreService } = useConfigure();
+
   const { caInfo, onClick } = props;
   const { alias, icon, selectedAvId, id } = caInfo;
   if (!selectedAvId) {
     return <></>
   }
-  const ca = configureService.getAttributeByAlias(alias);
+  const ca = configureCoreService.getAttributeByAlias(alias);
+  const { values } = ca;
+  const avSelected = values.find((av: IAttributeValue) => av.selected);
+
+  /*const { configureService } = useConfigure();
   const { values } = ca;
   const [menuOpen, setMenuOpen] = useState(false);
   const [avs] = useState(values);
   const avSelected = values.find((av: IAttributeValue) => av.selected);
-  const click = (av: IAttributeValue) => {
-    onClick.call(null, {ca, av});
-  };*/
+  ;*/
   return (
     <>
       <div className='fc-attribute-header' key={0}>
-        <div>
-          
-        </div>
-        <div>
-          <span className='fc-attribute-header--ca-name'>Temple</span>
-        </div>
-        <div >
-          <span className='fc-attribute-header--av-name'>Black</span>
+        <div className='fc-attribute-header--info'>
+          <div className='fc-attribute-header--info--image'>
+            <img src={`/img/${icon}.png`} alt={`configurable attribute ${icon}`} width={48} height={48} />
+          </div>
+          <div className='fc-attribute-header--info--ca-name'>
+            <span>{ca?.name}</span>
+          </div>
+          <div className='fc-attribute-header--info--av-name'>
+            <span>{avSelected?.name}</span>
+          </div>
         </div>
         <div className='fc-attribute-header--icon'>
-          <button
-            onClick={() => {}}
+          <button className='fc-attribute-header--icon--button'
+            onClick={() => { return setMenuOpen(!menuOpen); }}
             >
             {menuOpen ? 
-              (
-                <svg xmlns='http://www.w3.org/2000/svg' width='17' height='16' viewBox='0 0 17 16' fill='none'>
-                  <g clipPath='url(#clip0_3994_29036)'>
-                    <path d='M8.37888 12.943L0.574219 5.13798L1.51689 4.19531L8.37888 11.057L15.2409 4.19531L16.1836 5.13798L8.37888 12.943Z' fill='#1F1F24'/>
-                  </g>
-                  <defs>
-                    <clipPath id='clip0_3994_29036'>
-                      <rect width='16' height='16' fill='white' transform='translate(0.378906)'/>
-                    </clipPath>
-                  </defs>
-                </svg>
-              ) :
-              (
-                <svg xmlns='http://www.w3.org/2000/svg' width='17' height='16' viewBox='0 0 17 16' fill='none'>
-                  <g clipPath='url(#clip0_3994_28473)'>
-                    <path d='M8.37893 3.05702L16.1836 10.862L15.2409 11.8047L8.37893 4.94302L1.51693 11.8047L0.574261 10.862L8.37893 3.05702Z' fill='#1F1F24'/>
-                  </g>
-                  <defs>
-                    <clipPath id='clip0_3994_28473'>
-                      <rect width='16' height='16' fill='white' transform='matrix(-1 0 0 -1 16.3789 16)'/>
-                    </clipPath>
-                  </defs>
-                </svg>
-              )
+              (<svg xmlns='http://www.w3.org/2000/svg' width='17' height='16' viewBox='0 0 17 16' fill='none'>
+                <g clipPath='url(#clip0_3994_28473)'>
+                  <path d='M8.37893 3.05702L16.1836 10.862L15.2409 11.8047L8.37893 4.94302L1.51693 11.8047L0.574261 10.862L8.37893 3.05702Z' fill='#1F1F24'/>
+                </g>
+                <defs>
+                  <clipPath id='clip0_3994_28473'>
+                    <rect width='16' height='16' fill='white' transform='matrix(-1 0 0 -1 16.3789 16)'/>
+                  </clipPath>
+                </defs>
+              </svg>) :
+              (<svg xmlns='http://www.w3.org/2000/svg' width='17' height='16' viewBox='0 0 17 16' fill='none'>
+                <g clipPath='url(#clip0_3994_29036)'>
+                  <path d='M8.37888 12.943L0.574219 5.13798L1.51689 4.19531L8.37888 11.057L15.2409 4.19531L16.1836 5.13798L8.37888 12.943Z' fill='#1F1F24'/>
+                </g>
+                <defs>
+                  <clipPath id='clip0_3994_29036'>
+                    <rect width='16' height='16' fill='white' transform='translate(0.378906)'/>
+                  </clipPath>
+                </defs>
+              </svg>)
             }
           </button>
         </div>
--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -3,13 +3,26 @@ import { useSelector } from 'react-redux';
 import AttributeHeader from './components/attribute-header';
 
 import './index.css';
+import { ICAMap } from '../../constants';
 
 export default function Menu() {
   const name = useSelector((state: any) => state?.product?.name);
+  const casToRender = useSelector((state: any) => state?.ui?.casToRender);
   
   return (
     <section className={`fc-menu ${!name ? 'fc-skeleton' : ''}`}>
-      <AttributeHeader onClick={() => {}} />
+      {(casToRender && casToRender.length) && 
+        <ul className='fc-menu--list'>
+        {(casToRender && casToRender.length) && casToRender.map(
+            (caInfo: ICAMap) => {
+              return (
+                <li key={caInfo.id}>
+                  <AttributeHeader caInfo={caInfo} onClick={() => {}} key={caInfo.id}/>
+                </li>
+              );
+        })}
+        </ul>
+      }
     </section>
   );
 }
